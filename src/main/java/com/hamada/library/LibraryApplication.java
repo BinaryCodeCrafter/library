05970e1dda27a36652794af2a1ed7f1562cdfbf8
hamda <hamadhusindev@gmail.com>
2024-12-01 21:31:13 +0300

    Implemented basic authentication

--- src/main/java/com/hamada/library/LibraryApplication.java
+++ src/main/java/com/hamada/library/LibraryApplication.java
@@ -3,9 +3,11 @@ package com.hamada.library;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 @EnableCaching
+@EnableWebSecurity
 public class LibraryApplication {
 
 	public static void main(String[] args) {
